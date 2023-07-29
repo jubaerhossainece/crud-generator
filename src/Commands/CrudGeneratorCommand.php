3af94598507e06d9e885c75b5f57f82def6dee2e
jubaerhossainece <jubaerkuet@gmail.com>
2023-07-29 17:58:59 +0600

    all removed helper functions, used Str methods instead

--- src/Commands/CrudGeneratorCommand.php
+++ src/Commands/CrudGeneratorCommand.php
@@ -14,20 +14,20 @@ class CrudGeneratorCommand extends Command
     {
         $table = $this->argument('table');
         // Generate the model
-        $this->call('make:model', ['name' => 'Models/' . Str::studly(str_singular($table))]);
+        $this->call('make:model', ['name' => 'Models/' . Str::studly(Str::singular($value)($table))]);
 
         // Generate the controller
-        $this->call('make:controller', ['name' => 'Controllers/' . Str::studly(str_singular($table)) . 'Controller']);
+        $this->call('make:controller', ['name' => 'Controllers/' . Str::studly(Str::singular($value)($table)) . 'Controller']);
 
         // Generate the request
-        $this->call('make:request', ['name' => 'Requests/' . Str::studly(str_singular($table)) . 'Request']);
+        $this->call('make:request', ['name' => 'Requests/' . Str::studly(Str::singular($value)($table)) . 'Request']);
 
         // Generate the views
         // You can copy the Blade templates from the package's views directory to the Laravel project's views directory.
 
         // Generate the migration
         $this->call('make:migration', [
-            'name' => 'create_' . str_plural($table) . '_table',
+            'name' => 'create_' . Str::plural($table) . '_table',
             '--create' => $table,
         ]);
 
@@ -35,7 +35,7 @@ class CrudGeneratorCommand extends Command
         $this->call('migrate');
 
         // Append CRUD routes to routes/web.php
-        $routes = "Route::resource('" . str_plural($table) . "', '" . Str::studly(str_singular($table)) . "Controller');";
+        $routes = "Route::resource('" . Str::plural($table) . "', '" . Str::studly(Str::singular($value)($table)) . "Controller');";
 
         file_put_contents(base_path('routes/web.php'), $routes, FILE_APPEND);
 
